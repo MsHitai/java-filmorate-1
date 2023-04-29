a3b43f49dbc35876d246bffb00936555a4770f87
NikitaB <nik858ni@yandex.ru>
2023-04-29 23:05:45 +0300

    style: добавить пробел

--- src/main/java/ru/yandex/practicum/filmorate/exception/ErrorUserException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/ErrorUserException.java
@@ -1,6 +1,6 @@
 package ru.yandex.practicum.filmorate.exception;
 
-public class ErrorUserException extends Exception{
+public class ErrorUserException extends Exception {
     public ErrorUserException(String message) {
         super(message);
     }
